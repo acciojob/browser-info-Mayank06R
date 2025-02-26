e9e60f775b303e4ea131baa72b45b01980e7b7cc
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-26 10:13:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Get the browser name and version
+        var userAgent = navigator.userAgent;
+        var browserName = navigator.appName;
+        var fullVersion = '' + parseFloat(navigator.appVersion);
+        var majorVersion = parseInt(navigator.appVersion, 10);
+
+        // Display the browser information
+        var browserInfoDiv = document.getElementById('browser-info');
+        browserInfoDiv.textContent = "You are using " + browserName + " version " + fullVersion;
